60df37d8efb4b2ce378a1a6c6c2cda2ac5a87818
Malindu-D <mduwage@gmail.com>
2025-09-18 02:55:39 +0530

    add env variable

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Azure!'
+    message = os.getenv("FLASK_MESSAGE", "Welcome!")
+    return message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
