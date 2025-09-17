73b2348d711e787866930fd1d80452f1664a8c7d
Malindu-D <mduwage@gmail.com>
2025-09-18 03:05:39 +0530

    update greating

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    message = os.getenv("FLASK_MESSAGE", "Welcome!")
+    message = os.getenv("FLASK_MESSAGE", "Hi, Welcome!")
     return message
 
 if __name__ == '__main__':
